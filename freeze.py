816a6fc4ca95f0f2582f5ce425eabe43c8a3a722
Eli Gundry <eligundry@gmail.com>
2016-10-29 16:48:32 -0400

    Start converting to flask

--- freeze.py
+++ freeze.py
@@ -1,4 +1,9 @@
-from flask import Flask
+"""Static site generator for eligundry.com"""
+
+import os
+
+from flask import Flask, render_template
+from flask_flatpages import FlatPages
 from flask_frozen import Freezer
 
 
@@ -8,22 +13,41 @@ freezer = Freezer()
 
 def register_routes(app):
     """Factory method to register routes."""
+    # template_dir = 'templates/'
+    #
+    # for subdir, dirs, files in os.walk(template_dir):
+    #     base_route = subdir.split(template_dir)
+    #
+    #     if any((folder in base_route for folder in ('layouts', 'includes'))):
+    #         continue
+    #     elif base_route[0] in template_dir:
+    #         base_route[0] = ''
+    #
+    #     @app.route(route_name, methods='GET')
+    #     def index():
+    #         return "hay"
+
     @app.route('/')
+    def index():
+        return render_template('index.html')
 
 
 def create_app(settings):
     """App factory."""
     app = Flask(__name__)
+    app.config.from_object('config')
 
     # Flask Frozen
     # https://pythonhosted.org/Frozen-Flask/
     freezer.init_app(app)
 
-    # Register routes
+    register_routes(app)
 
+    # Register routes
     return app
 
 
 if __name__ == '__main__':
-    app_ = create_app(None)
+    app_ = create_app('config.py')
     freezer.freeze()
+    app_.run(debug=True)
