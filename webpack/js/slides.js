0e5a1aa08d6d80d084b4ec530388994149b2ae78
Eli Gundry <eligundry@gmail.com>
2017-06-11 19:45:33 -0400

    Add more content.

--- webpack/js/slides.js
+++ webpack/js/slides.js
@@ -1,14 +1,25 @@
-import Reveal from 'reveal.js';
+// @TODO Ugh this is nasty, but this is the only way that I can tell to get the
+// plugins working.
+window.Reveal = require('reveal.js/js/reveal.js');
+require('reveal.js/lib/js/head.min.js');
+require('reveal.js/lib/js/classList.js');
+require('reveal.js/plugin/markdown/marked.js');
+require('reveal.js/plugin/markdown/markdown.js');
 
 const attachSlideShow = () => {
     const slideShowContainer = document.querySelector('.reveal');
 
     if (slideShowContainer) {
-        Reveal.initialize({
-            width: '100%',
-            height: '100%'
-        });
+        createSlideShow();
     }
+};
+
+const createSlideShow = () => {
+    window.Reveal.initialize({
+        history: true,
+        width: '100%',
+        height: '100%',
+    });
 };
 
 export default attachSlideShow();
