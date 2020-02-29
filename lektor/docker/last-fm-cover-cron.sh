0b5052625d43fac1036f1a080454cba6887dcffb
Eli Gundry <eligundry@gmail.com>
2020-02-28 22:27:17 -0500

    Remove the old lektor site

--- lektor/docker/last-fm-cover-cron.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-curl -o /usr/share/nginx/html/last-fm.jpeg \
-    'http://tapmusic.net/collage.php?user=eli_pwnd&type=7day&size=3x3&caption=true'
-cp /usr/share/nginx/html/last-fm.jpeg /opt/eligundry.com/content/last-fm.jpeg
--- lektor/webpack/js/main.js
+++
@@ -1,7 +0,0 @@
-import attachPrintPageEvent from './print.js';
-import attachSlideShow from './slides.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    attachPrintPageEvent();
-    attachSlideShow();
-});
