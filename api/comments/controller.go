d720b95002e93509deeb9c58677db21999d780a8
Eli Gundry <eligundry@gmail.com>
2020-03-09 23:04:03 -0400

    Implement a comments RSS feed so that I can hook it up to IFTTT

--- api/comments/controller.go
+++ api/comments/controller.go
@@ -1,17 +1,21 @@
 package comments
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/feeds"
 )
 
 func RegisterRoutes(router *gin.RouterGroup) {
 	CreateTable()
 	comments := router.Group("/comments")
 	{
-		comments.POST("/*path", PostComment)
-		comments.GET("/*path", GetComments)
+		comments.POST("/path/*path", PostComment)
+		comments.GET("/path/*path", GetComments)
+		comments.GET("/feed.rss", GetCommentsFeed)
 	}
 }
 
@@ -49,7 +53,7 @@ func PostComment(c *gin.Context) {
 
 func GetComments(c *gin.Context) {
 	path := c.Param("path")
-	comments, err := GetCommentsByPath(path)
+	comments, err := GetCommentsByPath(path, false)
 
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
@@ -59,4 +63,45 @@ func GetComments(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, comments)
+}
+
+func GetCommentsFeed(c *gin.Context) {
+	comments, err := GetCommentsByPath("/", true)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	feed := &feeds.Feed{
+		Title: "Comments for eligundry.com",
+		Link: &feeds.Link{
+			Href: "https://eligundry.com/",
+		},
+		Description: "All the comments posted to the site",
+	}
+
+	for _, comment := range comments {
+		feed.Items = append(feed.Items, &feeds.Item{
+			Title: fmt.Sprintf("Comment on %s at %s", comment.Path, comment.PostedAt.Format(time.RFC3339)),
+			Link: &feeds.Link{
+				Href: fmt.Sprintf("https://eligundry.com%s#comment-%d", comment.Path, comment.ID),
+			},
+			Author: &feeds.Author{
+				Name:  comment.Email,
+				Email: comment.Email,
+			},
+			Description: comment.Comment,
+			Created:     comment.PostedAt,
+		})
+	}
+
+	rss, err := feed.ToRss()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Data(http.StatusOK, "application/rss+xml; charset=utf-8", []byte(rss))
 }
--- api/comments/data.go
+++ api/comments/data.go
@@ -2,6 +2,7 @@ package comments
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/eligundry/eligundry.com/api/common"
 )
@@ -55,14 +56,21 @@ func GetCommentByID(id int) (Comment, error) {
 	return comment, nil
 }
 
-func GetCommentsByPath(path string) ([]Comment, error) {
+func GetCommentsByPath(path string, descending bool) ([]Comment, error) {
 	comments := []Comment{}
 	db := common.GetDB()
-	err := db.Select(&comments, `
+	orderBy := ""
+
+	if descending {
+		orderBy = "ORDER BY posted_at DESC"
+	}
+
+	err := db.Select(&comments, fmt.Sprintf(`
         SELECT *
         FROM comments
         WHERE path LIKE ?
-    `, path+"%")
+        %s
+    `, orderBy), path+"%")
 
 	if err != nil && err != sql.ErrNoRows {
 		return comments, err
