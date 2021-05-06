91e11eaeb961e69db20350a0b4e5d3d527ce1274
Eli Gundry <eligundry@gmail.com>
2021-05-06 17:17:11 -0400

    Build site in Github Actions without Docker
    If we're gonna do Gatsby, buy into it's whole ecosystem.

--- scripts/deploy-assets.sh
+++ scripts/deploy-assets.sh
@@ -4,12 +4,6 @@ set -e
 
 COMPILED_PATH="./gatsby/public"
 
-# Copy files from the Docker container
-rm -rf $COMPILED_PATH
-docker run -d --rm --name=website eligundry/website.eligundry.com:latest
-docker cp website:/usr/share/nginx/html $COMPILED_PATH
-docker stop website
-
 # Digital Ocean Spaces does not serve content via gzip so I have to do
 # this my own damn self. The Gatsby build will precompress the files and this
 # will chop off the gz portion of it so, overwriting the non-compressed version
@@ -17,10 +11,4 @@ docker stop website
 for f in $COMPILED_PATH/*.gz; do mv -v -- "$f" "${f%.gz}"; done 
 
 # Sync all files to Digital Ocean Spaces
-aws s3 sync \
-    --endpoint="$DO_SPACES_ENDPOINT" \
-    --acl="public-read" \
-    --content-encoding "gzip" \
-    --cache-control "max-age=$(echo "60 * 60 * 24 * 7" | bc)" \
-    $COMPILED_PATH \
-    "s3://$DO_SPACES_BUCKET/site"
+npm --prefix=gatsby run deploy
