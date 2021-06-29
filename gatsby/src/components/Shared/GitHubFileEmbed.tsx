0d60adba0b160a2a3c291f4d901da4dcb001f843
Eli Gundry <eligundry@gmail.com>
2021-06-28 23:49:49 -0400

    Add GitHubFileEmbed component for use in blog posts

--- gatsby/src/components/Shared/GitHubFileEmbed.tsx
+++ gatsby/src/components/Shared/GitHubFileEmbed.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import useEffectOnce from 'react-use/lib/useEffectOnce'
+import postscribe from 'postscribe'
 
 interface Props {
   fileURL: string
 }
 
 const GitHubFileEmbed: React.FC<Props> = ({ fileURL }) => {
-  return null
+  const scriptTarget = useRef<HTMLDivElement>()
+
+  useEffectOnce(() => {
+    if (!scriptTarget.current) {
+      return
+    }
+
+    const query = new URLSearchParams({
+      target: fileURL,
+      style: 'github-gist',
+      showBorder: 'on',
+      showLineNumbers: 'on',
+      showFileMeta: 'on',
+    })
+
+    postscribe(
+      scriptTarget.current,
+      `<script async cross-origin="anonymous" src="https://emgithub.com/embed.js?${query.toString()}"></script>`
+    )
+  })
+
+  return <div ref={scriptTarget} />
 }
 
 export default GitHubFileEmbed
