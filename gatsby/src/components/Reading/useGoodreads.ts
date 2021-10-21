25065244ffd77d38593be3daf324aba07c794bff
Eli Gundry <eligundry@gmail.com>
2021-10-20 21:56:26 -0400

    ADHD Post (#85)
    * Add ADHD draft post
    
    * Add end of act 2 and most of act 3
    
    * ADHD post is close to completion
    
    Gotta figure out the ending and do some editing.
    
    * Finish the ADHD post
    
    * Update the ADHD post description

--- gatsby/src/components/Reading/useGoodreads.ts
+++ gatsby/src/components/Reading/useGoodreads.ts
@@ -17,7 +17,7 @@ export default function useGoodreadsShelf() {
             started
             coverImage {
               childImageSharp {
-                gatsbyImageData(width: 175)
+                gatsbyImageData(width: 175, quality: 90)
               }
             }
           }
@@ -36,7 +36,7 @@ export default function useGoodreadsShelf() {
             started
             coverImage {
               childImageSharp {
-                gatsbyImageData(width: 175)
+                gatsbyImageData(width: 175, quality: 90)
               }
             }
           }
