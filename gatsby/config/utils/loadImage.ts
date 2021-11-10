b2699eca1a4308bdef6ea481ee7f8cb3b392631a
Eli Gundry <eligundry@gmail.com>
2021-11-10 11:53:28 -0500

    Remove image caching

--- gatsby/config/utils/loadImage.ts
+++ gatsby/config/utils/loadImage.ts
@@ -10,25 +10,25 @@ interface LoadImageArgs extends CreateRemoteFileNodeArgs {
 const loadImage = async (args: LoadImageArgs) => {
   const { cacheKey, cache, ...createRemoteFileNodeArgs } = args
 
-  if (cacheKey) {
-    const cachedImage = await cache.get(cacheKey)
+  // if (cacheKey) {
+  //   const cachedImage = await cache.get(cacheKey)
 
-    if (cachedImage && cachedImage.fileNodeID) {
-      return cachedImage
-    }
-  }
+  //   if (cachedImage && cachedImage.fileNodeID) {
+  //     return cachedImage
+  //   }
+  // }
 
   const imageNode = await createRemoteFileNode({
     cache,
     ...createRemoteFileNodeArgs,
   })
 
-  if (imageNode && cacheKey) {
-    await cache.set(cacheKey, {
-      fileNodeID: imageNode.id,
-      modified: imageNode.modifiedTime,
-    })
-  }
+  // if (imageNode && cacheKey) {
+  //   await cache.set(cacheKey, {
+  //     fileNodeID: imageNode.id,
+  //     modified: imageNode.modifiedTime,
+  //   })
+  // }
 
   return imageNode
 }
