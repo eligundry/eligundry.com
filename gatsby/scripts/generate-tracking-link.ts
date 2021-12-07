9d71ae6a472d6a0c0e0bd8c1495be3225465a075
Eli Gundry <eligundry@gmail.com>
2021-12-06 21:29:00 -0500

    Add script to generate tracking links to recruiters I send emails to

---
+++ gatsby/scripts/generate-tracking-link.ts
@@ -0,0 +1,29 @@
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+const main = async () => {
+  const argv = await yargs(hideBin(process.argv)).options({
+    source: { type: 'string', default: 'recruiter-reply' },
+    medium: { type: 'string', default: 'gmail' },
+    company: { type: 'string' },
+    contactName: { type: 'string' },
+    content: { type: 'string', default: 'signature-link' },
+  }).argv
+
+  const p = new URLSearchParams({
+    utm_source: argv.source,
+    utm_medium: argv.medium,
+    utm_campaign: argv.company ?? argv.contactName ?? 'none',
+    utm_content: argv.content,
+  })
+
+  if (argv.company) {
+    p.set('utm_term', 'company')
+  } else if (argv.contactName) {
+    p.set('utm_term', 'person')
+  }
+
+  console.log(`https://eligundry.com/?${p.toString()}`)
+}
+
+main()
