78511b753b9966b979875462cedf3702058437bf
Eli Gundry <eligundry@gmail.com>
2022-04-02 00:22:37 -0400

    Switch to plaiceholder for blurred images

--- next/lib/utils.ts
+++ next/lib/utils.ts
@@ -1,17 +1,11 @@
-import canvas from 'canvas'
-import colorThief from 'color-thief-node'
+import { getPlaiceholder } from 'plaiceholder'
+import { cacheAxios } from './axios'
 
-const getPlaceholderForImage = async (imageURL: string): Promise<string> => {
-  const dominantColor = await colorThief
-    .getColorFromURL(imageURL)
-    .then((rgb) => `rgb(${rgb.join(', ')})`)
-  const can = canvas.createCanvas(1, 1)
-  const ctx = can.getContext('2d')
-  ctx.fillStyle = dominantColor
-  ctx.fillRect(0, 0, 1, 1)
-
-  return `data:image/png;base64,${can.toBuffer('image/png').toString('base64')}`
-}
+const getPlaceholderForImage = async (imageURL: string): Promise<string> =>
+  cacheAxios
+    .get(imageURL, { responseType: 'arraybuffer' })
+    .then((resp) => getPlaiceholder(Buffer.from(resp.data, 'binary')))
+    .then(({ base64 }) => base64)
 
 const api = { getPlaceholderForImage }
 
