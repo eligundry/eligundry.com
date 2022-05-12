8e97194dfc47dc415cf46b320323d2c4aafaa291
Eli Gundry <eligundry@gmail.com>
2022-05-11 20:51:51 -0400

    Initial commit with Astro

---
+++ astro/astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
+import turbolinks from "@astrojs/turbolinks";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+    // Enable the Preact integration to support Preact JSX components.
+    integrations: [preact(), tailwind(), turbolinks(), partytown(), sitemap()],
+});
+
