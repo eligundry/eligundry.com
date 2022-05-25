aff90174e5979b75cb90f234910283cc0148908d
Eli Gundry <eligundry@gmail.com>
2022-05-25 01:33:07 -0400

    Use a much smaller tailwind theme object

--- next/scripts/dumpTailwindConfig.ts
+++ next/scripts/dumpTailwindConfig.ts
@@ -1,9 +1,17 @@
 import fs from 'fs'
 import path from 'path'
 import resolveConfig from 'tailwindcss/resolveConfig'
+import pick from 'lodash/pick'
 import tailwindConfig from '../tailwind.config'
 
 const p = path.join('./', 'hooks', 'useTailwindTheme', 'theme.json')
 const fullConfig = resolveConfig(tailwindConfig)
-fs.writeFileSync(p, JSON.stringify(fullConfig.theme, undefined, 2))
-console.log(`Wrote tailwind config to ${p}`)
+fs.writeFileSync(
+  p,
+  JSON.stringify(
+    pick(fullConfig.theme, ['colors', 'screens', 'fontFamily', 'animation']),
+    undefined,
+    2
+  )
+)
+console.log(`Wrote tailwind theme to ${p}`)
