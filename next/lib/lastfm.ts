1a930bb5af00b1cfc6aac834b669891bd60265c5
Eli Gundry <eligundry@gmail.com>
2022-05-31 19:50:58 -0400

    Make lastfm cover fetching much more efficient

--- next/lib/lastfm.ts
+++ next/lib/lastfm.ts
@@ -25,49 +25,6 @@ const cache = new NodeCache({
   stdTTL: 60 * 60 * 1000,
 })
 
-const getScrobblesForWindow = async (
-  username: string,
-  start: Date,
-  end: Date = new Date()
-): Promise<RecentTrack[]> => {
-  const cachedScrobbles = cache.get<RecentTrack[]>(
-    `lastfm-scrobbles-${username}`
-  )
-
-  if (cachedScrobbles) {
-    return cachedScrobbles
-  }
-
-  const scrobbles: RecentTrack[] = []
-
-  const page = await lastfm.user.getRecentTracks(username, {
-    from: getUnixTime(start).toString(),
-    to: getUnixTime(end).toString(),
-  })
-
-  scrobbles.push(...page.tracks.filter((scrobble) => !scrobble.nowplaying))
-
-  const promises: Promise<RecentTrack[]>[] = []
-
-  for (let i = 1, { totalPages } = page.meta; i < totalPages; i++) {
-    promises.push(
-      lastfm.user
-        .getRecentTracks(username, {
-          from: getUnixTime(start).toString(),
-          to: getUnixTime(end).toString(),
-          page: i + 1,
-        })
-        .then((resp) => resp.tracks.filter(({ nowplaying }) => !nowplaying))
-    )
-  }
-
-  scrobbles.push(...(await Promise.all(promises)).flatMap((tracks) => tracks))
-
-  cache.set<RecentTrack[]>(`lastfm-scrobbles-${username}`, scrobbles)
-
-  return scrobbles
-}
-
 const getTopAlbumsCover = async (
   username: string
 ): Promise<LastFMCoverItem[]> => {
@@ -77,40 +34,27 @@ const getTopAlbumsCover = async (
     return topAlbums
   }
 
-  const scrobbles = await getScrobblesForWindow(
-    username,
-    subWeeks(new Date(), 1)
+  const albumsResp = await lastfm.user.getTopAlbums(username, {
+    limit: 15,
+    period: '7day',
+  })
+  topAlbums = await Promise.all(
+    albumsResp.albums
+      .filter((album) => album.image.length > 0)
+      .map(async (album) => ({
+        album: album.name,
+        artist: album.artist.name,
+        count: album.playcount,
+        cover: album.image.at(-1)?.url ?? '',
+        placeholder: await utils.getPlaceholderForImage(album.image[0].url),
+      }))
   )
-  const groupedScrobbles = groupBy(scrobbles, (scrobble) => scrobble.album.mbid)
-  topAlbums = (
-    await Promise.all(
-      Object.values(groupedScrobbles).map(
-        async (group): Promise<LastFMCoverItem> => ({
-          album: group[0].album.name,
-          artist: group[0].artist.name,
-          count: group.length,
-          cover: group[0].image.at(-1)?.url ?? '',
-          placeholder: await utils.getPlaceholderForImage(
-            group[0].image[0].url
-          ),
-        })
-      )
-    )
-  )
-    .sort((a, b) => {
-      if (b.count === a.count) {
-        return (b?.album ?? '') > (a?.album ?? '') ? 1 : -1
-      }
-
-      return b.count - a.count
-    })
-    .slice(1, 10)
 
   cache.set('lastfm-cover', topAlbums)
 
-  return topAlbums
+  return topAlbums.slice(0, 9)
 }
 
-const api = { getScrobblesForWindow, getTopAlbumsCover }
+const api = { getTopAlbumsCover }
 
 export default api
