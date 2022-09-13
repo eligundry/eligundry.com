7402d60d308af64d4c7eb17255eb797c61809143
Eli Gundry <eligundry@gmail.com>
2022-09-13 02:54:11 -0400

    Center align content (crappily)

--- 11ty/tailwind.config.js
+++ 11ty/tailwind.config.js
@@ -5,4 +5,12 @@ module.exports = {
   content: ["./**/*.{html,md,njk,liquid,js}", "!./_site", "!./node_modules"],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   safelist: ["tooltip", "tooltip-open"],
+  theme: {
+    container: {
+      screens: {
+        xl: "786px",
+        "2xl": "786px",
+      },
+    },
+  },
 };
