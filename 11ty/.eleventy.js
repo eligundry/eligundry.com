3855e0e63ea5b72441f7d656e046a0071c95c6fb
Eli Gundry <eligundry@gmail.com>
2022-09-13 01:16:37 -0400

    Wire up Goodreads book shelves

--- 11ty/.eleventy.js
+++ 11ty/.eleventy.js
@@ -56,13 +56,32 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
-  eleventyConfig.addShortcode("cachedToolTipImage", (metadata, alt) => {
-    return Image.generateHTML(metadata, {
-      alt,
-      loading: "lazy",
-      decoding: "async",
-      "data-tip": alt,
-      class: "tooltip",
-    });
-  });
+  eleventyConfig.addShortcode(
+    "cachedToolTipImage",
+    (metadata, alt, sizes = "") => {
+      const lowsrc = metadata.jpeg[0];
+      const sources = Object.values(metadata)
+        .map((imageFormat) => {
+          return `<source type="${
+            imageFormat[0].sourceType
+          }" srcset="${imageFormat
+            .map((entry) => entry.srcset)
+            .join(", ")}" sizes="${sizes}">`;
+        })
+        .join("\n");
+
+      return `
+      <picture class="tooltip" data-tip="${alt}">
+        ${sources}
+        <img
+          src="${lowsrc.url}"
+          width="${lowsrc.width}"
+          height="${lowsrc.height}"
+          alt="${alt}"
+          loading="lazy"
+          decoding="async">
+      </picture>
+    `;
+    }
+  );
 };
