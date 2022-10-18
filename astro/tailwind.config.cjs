cf90e0e0a3f6e5d4cd64b3cce1a107f9c74ccdb4
Eli Gundry <eligundry@gmail.com>
2022-10-17 22:11:21 -0400

    Initial commit with Astro… again

---
+++ astro/tailwind.config.cjs
@@ -0,0 +1,43 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/**
+ * @type {import('tailwindcss').Config}
+ */
+module.exports = {
+  content: ['./src/**/*.{html,astro,md,mdx}'],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  safelist: ['tooltip'],
+  theme: {
+    container: {
+      screens: {
+        xl: '786px',
+        '2xl': '786px',
+      },
+    },
+    screens: {
+      ...defaultTheme.screens,
+      print: {
+        raw: 'print',
+      },
+    },
+    extend: {
+      typography: ({ theme }) => ({
+        gray: {
+          css: {
+            a: {
+              color: theme('colors.primary'),
+            },
+            code: {
+              padding: 0,
+            },
+          },
+        },
+      }),
+    },
+  },
+  daisyui: {
+    themes: ['light', 'dark'],
+    darkTheme: 'dark',
+  },
+  darkMode: ['class', "[data-theme='dark']"],
+}
