1af0fa2c04b8be47827a25e563abb0415e48ff24
Eli Gundry <eligundry@gmail.com>
2023-03-05 04:14:25 -0500

    Fix a bunch of spacing and mobile issues

--- astro/src/config.ts
+++ astro/src/config.ts
@@ -7,7 +7,7 @@ export default {
   googleTagManagerID: 'GTM-W7KHFLM',
   lastFmUsername: 'eli_pwnd',
   spotifyPlaylist:
-    'https://open.spotify.com/embed/playlist/0ZuDSuihh3xRfCZN9jx5BD?utm_source=generator',
+    'https://open.spotify.com/embed/playlist/4BG565LHvSV26BzmNPobvD?utm_source=generator',
   goodreadsUserID: '29665939',
   github: 'eligundry',
 }
