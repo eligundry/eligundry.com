5d7cbfc8c496009bca9eaa46010d51481ba699f9
Eli Gundry <eligundry@gmail.com>
2023-07-25 18:38:43 -0400

    Disable builders

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,9 +19,7 @@ import netlify from '@astrojs/netlify/functions'
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: netlify({
-    builders: true,
-  }),
+  adapter: netlify(),
   experimental: {
     assets: true,
   },
